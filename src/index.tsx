14aba0a878920ab1a7e7c3e42347cc197938d28d
j- <j@skeoh.com>
2017-05-31 12:30:23 +1000

    Fetch top stories on init

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,13 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import createStore from './store/create';
 import { Provider } from 'react-redux';
+import { fetchTopStories } from './store/actions';
 
 const store = createStore();
+
+store.dispatch(
+  fetchTopStories()
+);
 
 ReactDOM.render(
   <Provider store={store}>
