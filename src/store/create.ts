523b53666c6213e71eeadd1843a0f6fb9a99f6e2
j- <j@skeoh.com>
2017-06-01 00:24:07 +1000

    Persist state to local storage

--- src/store/create.ts
+++ src/store/create.ts
@@ -9,14 +9,36 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from './root';
 
+const LOCAL_STORAGE_KEY: string = 'hn-state';
+
+const getState = () => {
+  try {
+    const stringified = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return JSON.parse(stringified || '');
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const setState = (state: {}) => {
+  try {
+    const stringified = JSON.stringify(state);
+    localStorage.setItem(LOCAL_STORAGE_KEY, stringified);
+  } catch (err) {
+    // Ignore
+  }
+};
+
 export default function createStore () {
   const middleware: Middleware[] = [
     ReduxThunk,
   ];
   const enhancers: GenericStoreEnhancer[] = [];
-  const store = createReduxStore(rootReducer, composeWithDevTools(
+  const initialState = getState();
+  const store = createReduxStore(rootReducer, initialState, composeWithDevTools(
     applyMiddleware(...middleware),
     ...enhancers
   ));
+  store.subscribe(() => setState(store.getState()));
   return store;
 }
