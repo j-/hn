83af5ffbe7d00f11758c09654b4d2356a6df356a
j- <j@skeoh.com>
2017-06-01 13:08:17 +1000

    Use `await` in API module properly

--- src/api.ts
+++ src/api.ts
@@ -1,36 +1,36 @@
 import { Item } from './types';
 
 export async function getItem (id: number): Promise<Item> {
-  return await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-    .then((res) => res.json());
+  const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+  return res.json();
 }
 
 export async function getTopStoryIds (): Promise<number[]> {
-  return await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-    .then((res) => res.json());
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+  return res.json();
 }
 
 export async function getNewStoryIds (): Promise<number[]> {
-  return await fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
-    .then((res) => res.json());
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json');
+  return res.json();
 }
 
 export async function getBestStoryIds (): Promise<number[]> {
-  return await fetch('https://hacker-news.firebaseio.com/v0/beststories.json')
-    .then((res) => res.json());
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/beststories.json');
+  return res.json();
 }
 
 export async function getAskStoryIds (): Promise<number[]> {
-  return await fetch('https://hacker-news.firebaseio.com/v0/askstories.json')
-    .then((res) => res.json());
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/askstories.json');
+  return res.json();
 }
 
 export async function getJobStoryIds (): Promise<number[]> {
-  return await fetch('https://hacker-news.firebaseio.com/v0/jobstories.json')
-    .then((res) => res.json());
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/jobstories.json');
+  return res.json();
 }
 
 export async function getShowStoryIds (): Promise<number[]> {
-  return await fetch('https://hacker-news.firebaseio.com/v0/showstories.json')
-    .then((res) => res.json());
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/showstories.json');
+  return res.json();
 }
