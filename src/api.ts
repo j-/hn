23428a1e92ad433079ee1b0ea5bc11f11317deaa
j- <j@skeoh.com>
2017-06-01 13:12:47 +1000

    Add quick docs to API module

--- src/api.ts
+++ src/api.ts
@@ -5,31 +5,37 @@ export async function getItem (id: number): Promise<Item> {
   return res.json();
 }
 
+/** Get 500 top story IDs */
 export async function getTopStoryIds (): Promise<number[]> {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
   return res.json();
 }
 
+/** Get 500 new story IDs */
 export async function getNewStoryIds (): Promise<number[]> {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json');
   return res.json();
 }
 
+/** Get 500 best story IDs */
 export async function getBestStoryIds (): Promise<number[]> {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/beststories.json');
   return res.json();
 }
 
+/** Get 200 ask IDs */
 export async function getAskStoryIds (): Promise<number[]> {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/askstories.json');
   return res.json();
 }
 
+/** Get 200 job IDs */
 export async function getJobStoryIds (): Promise<number[]> {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/jobstories.json');
   return res.json();
 }
 
+/** Get 200 show IDs */
 export async function getShowStoryIds (): Promise<number[]> {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/showstories.json');
   return res.json();
